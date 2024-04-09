65a34e2ad23293e4da3be228928051ae413b9a14
Flavie Bellis <flavie.bellis@etu.univ-st-etienne.fr>
2024-04-09 15:15:54 +0200

    filtre moyenneur

--- main.m
+++ main.m
@@ -2,12 +2,19 @@ clear all;
 close all;
 
 img = imread('img_test.jpg');
+img = imresize(img, [700 500]);
 figure(1)
-imshow(img);
+imshow(img); title('Image origniale');
+
 img_nvg = rgb2gray(img);
 figure(2)
-imshow(img_nvg);
+imshow(img_nvg); title('Image en nuances de gris');
 
-img_moy = filtreMoyenneur(img_nvg, 3)
+img_moy = filtreMoyenneur(img_nvg, 3);
 figure(3)
-imshow(img_moy);
+imshow(img_moy); title('Image avec filtrage moyenneur');
+
+seuil=90
+img_seuil=255*(img_moy<seuil);
+figure(4)
+imshow(img_seuil); title('Image filtrée');
